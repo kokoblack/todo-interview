bd0bb7109129d7efeb9b1fbf02e3a783557f73ee
kokoblack <benzimakareem@yahoo.com>
2023-04-01 08:34:15 +0100

    fix item wont delete

--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -1,53 +1,54 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type initialState = {
-    name: string
-    ifChecked: boolean
-    id: number
-}[]
+  name: string;
+  ifChecked: boolean;
+  id: number;
+}[];
 
 type todo = {
-    name: string,
-    ifChecked: boolean,
-    id: number
-}
+  name: string;
+  ifChecked: boolean;
+  id: number;
+};
 
-const initialState: initialState = []
+const initialState: initialState = [];
 
 const todoSlice = createSlice({
-    name: 'todos',
-    initialState,
-    reducers: {
-        add: (state, action: PayloadAction<todo>) => {
-            state.push(action.payload)
-        }, 
-        del: (state, action: PayloadAction<string>) => {
-            const name = state.map((e) => e.name)
-            const index = name.indexOf(action.payload)
-            index === 0 ? state.splice(index) : state.splice(index, index)
-        },
-        multipleDel: (state) => {
-            state.filter((todo) => {
-                console.log('dont know what is wrong')
-                return todo.ifChecked !== true
-                
-            })
-            
-        },
-        update: (state, action: PayloadAction<{name: string, checked: boolean}>) => {
-            const name = action.payload.name
-            state.map((e) => {
-                if (name === e.name) {
-                    return e.ifChecked = action.payload.checked
-                }
-            })
-        },
-        getLocalStorageTodo: (state, action: PayloadAction<initialState>) => {
-            state = action.payload
-        }
+  name: "todos",
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<todo>) => {
+      state.push(action.payload);
     },
-    
-})
+    del: (state, action: PayloadAction<string>) => {
+      const name = state.map((e) => e.name);
+      const index = name.indexOf(action.payload);
+      index === 0 && state.length >= 1
+        ? state.splice(index, index + 1)
+        : state.splice(index, index);
+    },
+    multipledel: (state) => {
+      const unChecked = state.filter((todo) => {
+        return todo.ifChecked !== true;
+      });
+      state = JSON.parse(JSON.stringify(unChecked));
+      console.log(state)
+    },
+    update: (
+      state,
+      action: PayloadAction<{ name: string; checked: boolean }>
+    ) => {
+      const name = action.payload.name;
+      state.map((e) => {
+        if (name === e.name) {
+          return (e.ifChecked = action.payload.checked);
+        }
+      });
+    },
+  },
+});
 
-export default todoSlice.reducer
-export const {add, del, multipleDel, update, getLocalStorageTodo} = todoSlice.actions
+export default todoSlice.reducer;
+export const { add, del, multipledel, update, } =
+  todoSlice.actions;
